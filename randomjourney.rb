d80315d5a1071fc6fdccd9f94eeb76bd215a2019
Daniel Miller <dealingwith@gmail.com>
2020-01-14 12:04:04 -0600

    fix domain & one test run

--- randomjourney.rb
+++ randomjourney.rb
@@ -7,7 +7,7 @@ old_links = File.readlines(File.dirname(__FILE__) + '/links.txt')
 
 mechanize = Mechanize.new
 # use machanize to get archives page content
-page = mechanize.get('http://www.danielsjourney.com/archives.html')
+page = mechanize.get('http://daniel.industries/archives')
 # get all the links in the page with the class 'archive_link'
 link = page.links_with(class: 'archive_link').sample
 # make sure it's a new one (not listed in links.txt)
@@ -19,7 +19,7 @@ link_href = link.href
 # get the text of the link
 link_text = link.text
 # create a new html string for the link
-puts link_tag = "<a href=\"http://danielsjourney.com#{link_href}\">#{link_text}</a>"
+puts link_tag = "<a href=\"http://daniel.industries#{link_href}\">#{link_text}</a>"
 
 # mailgun email sending
 mg_client = Mailgun::Client.new MAILGUN_API_KEY
